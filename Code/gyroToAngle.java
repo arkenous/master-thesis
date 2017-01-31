6d9519655ca7b2288ccc3f806774161070ffb09f
Kensuke Kosaka <k@trileg.net>
2017-01-31 15:30:45 +0900

    Update System

---
+++ Code/gyroToAngle.java
@@ -0,0 +1,12 @@
+public double[][][] gyroToAngle(double[][][] inputVal, double t) {
+  log(INFO);
+
+  double[][][] returnVal = new double[inputVal.length][NUM_AXIS][inputVal[0][0].length];
+
+  for (int time = 0; time < inputVal.length; time++)
+    for (int axis = 0; axis < NUM_AXIS; axis++)
+      for (int item = 0; item < inputVal[time][axis].length; item++)
+        returnVal[time][axis][item] = (inputVal[time][axis][item] * t);
+
+  return returnVal;
+}
