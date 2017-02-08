bb3ab752fb67b59a6bc0ea90a2f28864edd1f999
Kensuke Kosaka <k@trileg.net>
2017-02-08 12:45:46 +0900

    Update System of thesis

--- thesis/Code/dA-noise.cpp
+++ thesis/Code/dA-noise.cpp
@@ -1,29 +1,15 @@
-/**
- * データごとに0.0以上1.0未満の乱数を生成し，rate未満であればそのデータを0.0にする
- * @param input ノイズをのせるデータ
- * @param rate ノイズをのせる確率
- * @return ノイズをのせたデータ
- */
-vector<vector<double>> add_noise(const vector<vector<double>> &input, const float rate) {
+vector<vector<double>> gaussian_noise(const vector<vector<double>> &input, const double mean, const double stddev, const float rate) {
+  vector<vector<double>> result(input);
   random_device rnd;
   mt19937 mt;
   mt.seed(rnd());
   uniform_real_distribution<double> real_rnd(0.0, 1.0);
-  double rnd_value = 0.0;
-
-  vector<vector<double>> result(input);
+  std::normal_distribution<double> dist(mean, stddev);
 
   for (unsigned long i = 0, i_s = result.size(); i < i_s; ++i) {
-    double max = *std::max_element(result[i].begin(), result[i].end());
-    double min = *std::min_element(result[i].begin(), result[i].end());
     for (unsigned long j = 0, j_s = result[i].size(); j < j_s; ++j) {
-      rnd_value = real_rnd(mt);
-      if (rnd_value < rate) {
-        if (rnd_value < rate / 2) {
-          result[i][j] = min;
-        } else {
-          result[i][j] = max;
-        }
+      if (real_rnd(mt) <= rate) {
+        result[i][j] += dist(mt);
       }
     }
   }
